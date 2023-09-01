ff92676a8f7caecaa110862057bb2b6ed3d2d2d8
zeyad2003 <zeyadnasef85@gmail.com>
2023-09-01 04:32:31 +0300

    Update gitignore file

--- src/main/java/org/fawry/Practice.java
+++ src/main/java/org/fawry/Practice.java
@@ -1,7 +1,14 @@
 package org.fawry;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.Calendar;
+
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(ZonedDateTime.now());
     }
 }
